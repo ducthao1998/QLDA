46bd148435fc2efe3606e790800757f34292d687
ducthao1998 <thao.nd241072m@sis.hust.edu.vn>
2025-04-26 01:02:22 +0700

    sua login

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,25 +1,29 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { cookies } from "next/headers";
-export const createClient = async (cookieStore: ReturnType<typeof cookies>) => {
-    const cookies = await cookieStore;
-    return createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return cookies.getAll()
-          },
-          setAll(cookiesToSet) {
-            try {
-              cookiesToSet.forEach(({ name, value, options }) => cookies.set(name, value, options))
-            } catch {
-              // The [setAll](cci:1://file:///e:/laptrinh/QLLDA/QLDA/src/lib/supabase/server.ts:13:8-21:9) method was called from a Server Component.
-              // This can be ignored if you have middleware refreshing
-              // user sessions.
-            }
-          },
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function createClient() {
+  const cookieStore = await cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
-    );
-  };
+    }
+  )
+}
